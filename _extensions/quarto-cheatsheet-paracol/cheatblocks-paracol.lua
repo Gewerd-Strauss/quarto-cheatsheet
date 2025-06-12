e8d89513efb2977bac51ced784c9c4259b3c2aeb
Gewerd-Strauss <80548781+Gewerd-Strauss@users.noreply.github.com>
2025-06-13 00:41:07 +0200

    paracol-format: implement fontsize-control for cheatbody

--- _extensions/quarto-cheatsheet-paracol/cheatblocks-paracol.lua
+++ _extensions/quarto-cheatsheet-paracol/cheatblocks-paracol.lua
@@ -1,11 +1,14 @@
 local column_count = 4
 local use_paracol = true
+local cheat_fontsize = "small"
 local blocks = {}
 
 function Meta(meta)
   local fmt = meta['quarto-cheatsheet-paracol-pdf'] or meta['quarto-cheatsheet-pdf']
   print(meta["numcols"]) -- this prints the actual string I provide in the YAML
   print(column_count) -- this prints hte current value of `column_count` - which is the `local column_count = 4`
+  cheat_fontsize = meta['cheat-fontsize']
+  print(cheat_fontsize)
   column_count = 3 -- this successfully overwrites 
   column_count = tonumber(pandoc.utils.stringify(meta["numcols"]))
 
@@ -43,7 +46,9 @@ function Pandoc(doc)
     for _,b in ipairs(cols[i]) do
       local t = b.attributes.title or ""
       local c = pandoc.write(pandoc.Pandoc(b.content), "latex")
-      local box = string.format("\\begin{tcolorbox}[cheatbox,title={%s}]\n%s\n\\end{tcolorbox}", t, c)
+      local fontsize_str = pandoc.utils.stringify(cheat_fontsize)
+      local fontcmd = "\\" .. fontsize_str:lower()  -- converts "HUGE" to "\huge" (and equivalents) (LaTeX command) 
+      local box = string.format("\\begin{tcolorbox}[cheatbox, fontupper={%s},title={%s}]\n%s\n\\end{tcolorbox}",  fontcmd,t, c)
       table.insert(out, pandoc.RawBlock("latex", box))
     end
   end
